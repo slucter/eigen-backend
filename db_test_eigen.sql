1f91330d6283a18a65b2ca784f64b4ce4113712d
Muhamad Irhashdianto <irhashjeh@gmail.com>
2024-05-11 03:46:45 +0700

    add sql file

--- db_test_eigen.sql
+++ db_test_eigen.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE `be_test_eigen` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
 -- be_test_eigen.books definition
 
 CREATE TABLE `books` (
